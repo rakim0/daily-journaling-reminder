dea5f0a8b2ee8cbbf90bb23d8943d11a395d9145
Rakim Middya <103818757+rakim0@users.noreply.github.com>
2026-04-18 12:49:10 +0530

    chore: apply remaining project updates

--- scripts/prepare-build.mjs
+++ scripts/prepare-build.mjs
@@ -5,5 +5,5 @@ const filesToCopy = ["manifest.json", "styles.css", "versions.json"];
 await mkdir("build", { recursive: true });
 
 for (const file of filesToCopy) {
-	await copyFile(file, `build/${file}`);
+  await copyFile(file, `build/${file}`);
 }
